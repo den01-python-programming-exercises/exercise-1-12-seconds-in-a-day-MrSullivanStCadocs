88b0e938ad746f9c6a1a9a760cb9de8b0b5d59df
MrSullivan <rsullivan@stcadocs.com>
2021-02-09 13:46:14 +0000

    added variables, and calculation in parenthesis.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #write your code below this line
+    number_in_days = int(input("How many days would you like to convert into seconds? "))
+    number_in_seconds = (((number_in_days * 24) * 60) * 60)
+    print(number_in_seconds)
 
 if __name__ == '__main__':
     main()
